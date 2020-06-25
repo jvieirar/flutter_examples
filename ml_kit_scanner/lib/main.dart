6ff42fe3b6f3b9c8d70afecf565b8402845aeeda
Juan <jmvieirar@gmail.com>
2020-06-25 18:02:58 +1000

    barcode scan test 1

--- ml_kit_scanner/lib/main.dart
+++ ml_kit_scanner/lib/main.dart
@@ -1,100 +1,160 @@
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+void main() => runApp(MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      home: ScanApp(),
+    ));
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
+class ScanApp extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ScanAppState createState() => _ScanAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ScanAppState extends State<ScanApp> {
+  // properties
+  File _imageFile;
+  List<Face> _faces;
+  List<Barcode> _barcodes;
+  final ImagePicker _picker = ImagePicker();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  // constructors
+
+  // methods
+  void _getImageAndDetectFaces() async {
+    final imageFile = await _picker.getImage(source: ImageSource.gallery);
+    final image = FirebaseVisionImage.fromFile(File(imageFile.path));
+    final faceDetector = FirebaseVision.instance.faceDetector(
+      FaceDetectorOptions(
+        mode: FaceDetectorMode.accurate,
+      ),
+    );
+    final faces = await faceDetector.processImage(image);
+
+    // update state only if app mounted
+    if (mounted) {
+      setState(() {
+        _imageFile = File(imageFile.path);
+        _faces = faces;
+      });
+    }
   }
 
-  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
-    return ListTile(
-        title: Row(
-          children: <Widget>[
-            Expanded(
-              child: Text(
-                document['name'] != null ? document['name'] : 'Default',
-                style: Theme.of(context).textTheme.headline3,
-              ),
-            ),
-            Container(
-              decoration: const BoxDecoration(
-                color: Color(0xffddddff),
-              ),
-              padding: const EdgeInsets.all(10.0),
-              child: Text(
-                document['votes'] != null
-                    ? document['votes'].toString()
-                    : 'Default',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            )
-          ],
-        ),
-        onTap: () {
-          Firestore.instance.runTransaction((transaction) async {
-            DocumentSnapshot freshSnap =
-                await transaction.get(document.reference);
-            await transaction.update(freshSnap.reference, {
-              'votes': freshSnap['votes'] + 1,
-            });
-          });
-        });
+  void _getImageAndDetectBarcodes() async {
+    final imageFile = await _picker.getImage(source: ImageSource.gallery);
+    final image = FirebaseVisionImage.fromFile(File(imageFile.path));
+    final barcodeDetector = FirebaseVision.instance.barcodeDetector(
+      BarcodeDetectorOptions(barcodeFormats: BarcodeFormat.code128),
+    );
+    final barcodes = await barcodeDetector.detectInImage(image);
+    print('barcodes detected: ${barcodes.length}');
+    print(barcodes != null && barcodes.length > 0
+        ? '${barcodes[0].rawValue}'
+        : 'No barcodes');
+
+    // update state only if app mounted
+    if (mounted) {
+      setState(() {
+        _imageFile = File(imageFile.path);
+        _barcodes = barcodes;
+      });
+    }
   }
 
+  // render
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Scann App'),
       ),
-      body: StreamBuilder(
-        stream: Firestore.instance.collection('bandnames').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) return const Text('Loading...');
-          return ListView.builder(
-            itemExtent: 80.0,
-            itemCount: snapshot.data.documents.length,
-            itemBuilder: (context, index) =>
-                _buildListItem(context, snapshot.data.documents[index]),
-          );
-        },
+      body: Center(
+        child: _imageFile != null && _barcodes != null
+            ? Column(
+                children: <Widget>[
+                  Image.file(
+                    _imageFile,
+                  ),
+                  ..._barcodes.map(
+                      (barcode) => ListTile(title: Text(barcode.displayValue))),
+                ],
+              )
+            : Text('Select an image'),
       ),
+      // body: ImageAndFaces(
+      //   imageFile: _imageFile,
+      //   faces: _faces,
+      // ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _getImageAndDetectBarcodes,
+        tooltip: 'Pick an image',
+        child: Icon(Icons.add_a_photo),
+      ),
     );
   }
 }
+
+// class ImageAndFaces extends StatelessWidget {
+//   // properties
+//   final File imageFile;
+//   final List<Face> faces;
+
+//   // constructors
+//   ImageAndFaces({this.imageFile, this.faces});
+
+//   // methods
+
+//   // render
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: <Widget>[
+//         Flexible(
+//           flex: 2,
+//           child: Container(
+//             constraints: BoxConstraints.expand(),
+//             child: imageFile != null
+//                 ? Image.file(
+//                     imageFile,
+//                     // fit: BoxFit.cover,
+//                   )
+//                 : Text(''),
+//           ),
+//         ),
+//         Flexible(
+//           flex: 1,
+//           child: ListView(
+//             children: faces != null
+//                 ? faces.map((f) => FaceCoordinates(f)).toList()
+//                 : <Widget>[],
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+// class FaceCoordinates extends StatelessWidget {
+//   // properties
+//   final Face face;
+
+//   // constructors
+//   FaceCoordinates(this.face);
+
+//   // methods
+
+//   // render
+//   @override
+//   Widget build(BuildContext context) {
+//     final pos = face.boundingBox;
+//     return ListTile(
+//       title: Text('(${pos.top}, ${pos.left}), (${pos.bottom}, ${pos.right})'),
+//     );
+//   }
+// }
