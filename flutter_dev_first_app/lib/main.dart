eb4741693644f723b3cc69405d50012d34922b46
Juan <jmvieirar@gmail.com>
2020-06-26 19:31:58 +1000

    favorites added

--- flutter_dev_first_app/lib/main.dart
+++ flutter_dev_first_app/lib/main.dart
@@ -9,30 +9,59 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   // properties
-  var _wordPair = WordPair.random();
+  final _suggestions = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+  final _saved = Set<WordPair>();
 
   // constructors
 
   // methods
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
+    return ListTile(
+      title: Text(pair.asPascalCase, style: _biggerFont),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
 
   // render
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Main App',
+      title: 'Startup name generator',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Main App Text'),
+          title: Text('Startup name generator'),
         ),
-        body: Center(
-          child: Text(_wordPair.asPascalCase),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.refresh),
-          onPressed: () => setState(
-            () => _wordPair = WordPair.random(),
-          ),
-        ),
+        body: _buildSuggestions(),
       ),
     );
   }
